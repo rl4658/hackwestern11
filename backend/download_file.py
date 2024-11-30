aba20bec60ce5c5b009da56a61e5fcd06017d78f
PconnorsUWO <124845606+PconnorsUWO@users.noreply.github.com>
2024-11-30 15:48:48 -0500

    Update download_file.py

--- backend/download_file.py
+++ backend/download_file.py
@@ -24,7 +24,3 @@ def download_file():
         return jsonify({"error": "File not found"}), 404
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-if __name__ == '__main__':
-    # Run the app on localhost
-    app.run(debug=True)
